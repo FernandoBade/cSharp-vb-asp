0e74798827f92ada2cd9105c0a4f4e49aeec871a
FernandoBade <bade.fernando@gmail.com>
2022-08-28 15:09:15 -0300

    Update

---
+++ cSharp-basics/TypeConversion/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            int i = 100000;
+                        byte b = (byte) i;
+                        Console.WriteLine(i);*/
+
+            try
+            {
+                /*                var number = "1234";
+                                byte b = Convert.ToByte(number);
+                                Console.WriteLine(b);*/
+
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("O número não pode ser covertido para o tipo 'byte'.");                
+            }
+
+
+        }
+    }
+}
---
+++ cSharp-basics/Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            byte number = 2;
+                        int count = 10;
+                        float totalPrice = 20.95f;
+                        char character = 'F';
+                        string firstName = "Fernando";
+                        bool isWorking = false;*/
+
+            /*            var number = 2;
+                        var count = 10;
+                        var totalPrice = 20.95f;
+                        var character = 'F';
+                        var firstName = "Fernando";
+                        var isWorking = false;*/
+
+            /*            Console.WriteLine(number);
+                        Console.WriteLine(count);
+                        Console.WriteLine(totalPrice);
+                        Console.WriteLine(character);
+                        Console.WriteLine(firstName);
+                        Console.WriteLine(isWorking);*/
+
+            /*            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+                        Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);*/
+
+            const float Pi = 3.14f;
+        }
+    }
+}
